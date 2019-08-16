e4a2c5627f8fb80b58cd1ac5cbfd77d8bdb46a81
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-08-16 00:23:12 -0500

    Upgrade game with looping through questions till exit and counting score

---
+++ script2.js
@@ -0,0 +1,64 @@
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+(function () {
+    var score = 0;
+
+    function Question (question, answers, correctAnswer) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question);
+
+        for(var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    };
+
+    Question.prototype.checkAnswer = function (answer) {
+        if (parseInt(answer) === this.correctAnswer) {
+            console.log("Correct answer!");
+            score += 1;
+        } else if (answer === 'exit'){
+            return;
+        } else {
+            console.log("Wrong answer. Try harder!");
+        }
+
+        this.displayScore();
+    };
+
+    Question.prototype.displayScore = function () {
+        console.log('Your current score is: ' + score);
+        console.log('-------------------------');
+    };
+
+    var question1 = new Question("Is JavaScript the coolest programming language in the world?", ["Yes", "No"], 0);
+    var question2 = new Question("What is the name of the founder of Newton's laws?", ["John", "Adam", "Isaac"], 2);
+    var question3 = new Question("What is the capital of Texas state?", ["Houston", "Dallas", "San Antonio", "Austin"], 3);
+
+    var questions = [question1, question2, question3];
+
+    do {
+        var n = Math.floor(Math.random() * questions.length);
+
+        questions[n].displayQuestion();
+
+        var answer = prompt('Select correct answer (just type the number). Or type exit to quit.');
+
+        questions[n].checkAnswer(answer);
+
+    } while (answer !== 'exit');
+})();
